7709f931b42b8ca18c9763c437b93b88870912c5
Maestro1982 <kris.tote@telenet.be>
2023-07-04 17:05:57 +0200

    modify layout.tsx app dashboard storeId folder, page.tsx app dashboard storeId routes, create main-nav.tsx, navbar.tsx, store-switcher.tsx in components folder

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,4 +1,5 @@
 import prismadb from '@/lib/prismadb';
+import Navbar from '@/components/navbar';
 
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
@@ -29,7 +30,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a navbar</div>
+      <Navbar />
       {children}
     </>
   );
