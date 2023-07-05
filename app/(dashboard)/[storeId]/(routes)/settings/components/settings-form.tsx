e24677d19aaa0973d33b4f5758f4ec86a03d8a42
Maestro1982 <kris.tote@telenet.be>
2023-07-05 13:50:20 +0200

    create page.tsx app dashboard storeId routes settings folder, settings-form.tsx app dashboard storeId routes settings components folder, route.ts app api stores storeId folder, alert-modal.tsx components modals folder

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import * as z from 'zod';
+import axios from 'axios';
+import { Store } from '@prisma/client';
+import { Trash } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'react-hot-toast';
+import { useParams, useRouter } from 'next/navigation';
+
+import { Heading } from '@/components/ui/heading';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { AlertModal } from '@/components/modals/alert-modal';
+
+interface SettingsFormStoreProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: React.FC<SettingsFormStoreProps> = ({
+  initialData,
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('Store updated successfully.');
+    } catch (error) {
+      toast.error('Something went wrong.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted successfully.');
+    } catch (error) {
+      toast.error(
+        'First make sure you have removed all the products and categories from your store.'
+      );
+    } finally {
+      setIsLoading(false);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={onDelete}
+        isLoading={isLoading}
+      />
+      <div className='flex items-center justify-between'>
+        <Heading title='Settings' description='Manage store preferences' />
+        <Button
+          disabled={isLoading}
+          variant='destructive'
+          size='icon'
+          onClick={() => setIsOpen(true)}
+        >
+          <Trash className='w-4 h-4' />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-full'
+        >
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder='Store name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} className='ml-auto' type='submit'>
+            Save changes
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
