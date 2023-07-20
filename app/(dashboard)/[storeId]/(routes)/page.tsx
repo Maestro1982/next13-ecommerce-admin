71b37a7a800ed51a69392b5620898112b4d98b2f
Maestro1982 <kris.tote@telenet.be>
2023-07-20 12:49:29 +0200

    create get-graph-revenue.ts, get-sales-count.ts, get-stock-count.ts , get-total-revenue.ts in actions folder, create overview.tsx in components folder, modify page.tsx in app dashboard storeId routes folder

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -3,15 +3,25 @@ import { CreditCard, Euro, Package } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
+import { Overview } from '@/components/overview';
 
-import prismadb from '@/lib/prismadb';
 import { formatter } from '@/lib/utils';
+
+import { getTotalRevenue } from '@/actions/get-total-revenue';
+import { getSalesCount } from '@/actions/get-sales-count';
+import { getStockCount } from '@/actions/get-stock-count';
+import { getGraphRevenue } from '@/actions/get-graph-revenue';
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
+
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
@@ -26,7 +36,9 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <Euro className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-2xl font-bold'>{formatter.format(129)}</div>
+              <div className='text-2xl font-bold'>
+                {formatter.format(totalRevenue)}
+              </div>
             </CardContent>
           </Card>
           <Card>
@@ -35,7 +47,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <CreditCard className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-2xl font-bold'>+ 25</div>
+              <div className='text-2xl font-bold'>+{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
@@ -46,10 +58,18 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
               <Package className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
             <CardContent>
-              <div className='text-2xl font-bold'>10</div>
+              <div className='text-2xl font-bold'>{stockCount}</div>
             </CardContent>
           </Card>
         </div>
+        <Card className='col-span-4'>
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className='pl-2'>
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
